587bd3acb05e391a513eab20a64b1bf138258b17
Botshelo Tlhabanyane <79863670+BotsheloT@users.noreply.github.com>
2022-08-06 23:20:43 +0200

    Final Program

--- ICE Tasks/ICE Task 1/ICETask1/Program.cs
+++ ICE Tasks/ICE Task 1/ICETask1/Program.cs
@@ -1,3 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Array for input data
+int[] nums = new int[3];
+
+//For loop for user input
+for (int i = 0; i < 3; i++)
+{
+    Console.WriteLine("Please enter a number");
+    nums[i] = int.Parse(Console.ReadLine());
+}
+
+//Variable for sum of even numbers
+int sum = 0;
+
+//Check for even numbers
+foreach (int num in nums)
+{
+    if (num%2 == 0)
+    {
+        sum += num;
+    }
+}
+
+//Final results
+Console.WriteLine("Sum of all even numbers stored in the array is: {0}", sum);
+
 
