647413c00a6b2774209f192c76238452e5c2c40f
Botshelo Tlhabanyane <79863670+BotsheloT@users.noreply.github.com>
2022-08-08 10:26:04 +0200

    Random Array exercise

---
+++ Arrays/Program.cs
@@ -0,0 +1,18 @@
+﻿//Array for 10 numbers
+int[] nums = new int[10];
+
+//Variable for random numbers
+Random ran = new Random();
+
+/*foreach (int num in nums)
+{
+    Random ran = new Random();
+    num = ran.Next(0, 100);
+}
+Foreach only works for objects that that already have an assigned value*/
+
+for (int i = 0; i < 10; i++)
+{    
+    nums[i] = ran.Next(0, 100);
+}
+
