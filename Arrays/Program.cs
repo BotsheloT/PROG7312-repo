0e0205e953fc945e5e2d24dd72d03b0f2d94b251
Botshelo Tlhabanyane <79863670+BotsheloT@users.noreply.github.com>
2022-08-08 10:32:56 +0200

    List for random numbers

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -1,5 +1,12 @@
-﻿//Array for 10 numbers
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+//Array for 10 numbers
 int[] nums = new int[10];
+
+//List for 10 numbers
+List<int> numbers = new List<int>();
 
 //Variable for random numbers
 Random ran = new Random();
@@ -13,6 +20,8 @@ Foreach only works for objects that that already have an assigned value*/
 
 for (int i = 0; i < 10; i++)
 {    
-    nums[i] = ran.Next(0, 100);
+    nums[i] = ran.Next(1, 100);
+    numbers.Add(ran.Next(1, 100));
 }
 
+numbers.Sort();
