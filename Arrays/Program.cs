5d37a9f9b6492de5f42e2a77a65b1fe291e60c29
Botshelo Tlhabanyane <79863670+BotsheloT@users.noreply.github.com>
2022-08-08 10:48:59 +0200

    Sorted List code

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -25,3 +25,27 @@ for (int i = 0; i < 10; i++)
 }
 
 numbers.Sort();
+
+//Sorted list for testing
+/*This functions as a key value pair collection*/
+SortedList<int, int> kvpNums = new SortedList<int, int>();
+
+/*Ensure that you follow the data types that are Assoctiated with the key and 
+*its value. Keys cannot be repeated either*/
+kvpNums.Add(6, 14);
+kvpNums.Add(2, 81);
+kvpNums.Add(3, 7);
+kvpNums.Add(4, 10);
+kvpNums.Add(5, 65);
+
+//Displaying the values
+foreach (KeyValuePair<int, int> item in kvpNums)
+{
+    Console.WriteLine(item);
+}
+
+//Displaying the values in the KVP only
+foreach (int val in kvpNums.Values)
+{
+    Console.WriteLine(val);
+}
