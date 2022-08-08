0658eb818cedf8077c4fed40a1d5b9a85f82650b
Botshelo Tlhabanyane <79863670+BotsheloT@users.noreply.github.com>
2022-08-08 11:38:43 +0200

    Bubble Sort

---
+++ Sorting/Program.cs
@@ -0,0 +1,42 @@
+﻿/*Types of sorting algorithms:
+ Insertion
+ Bubblue
+ Selection*/
+int[] nums = new int[10];
+
+//Random variable
+Random rndm = new Random();
+
+for (int i = 0; i < 10; i++)
+{
+    nums[i] = rndm.Next(1, 2000);
+    Console.WriteLine(nums[i]);
+}
+
+//Funstion for Bubble sort
+var bSort = (int[] colls) =>
+{
+    //Temp value for sorting
+    int temp;
+    for (int i = 0; i < colls.Length-1; i++)
+    {
+        for (int j = i++; j < colls.Length; j++)
+        {
+            if (colls[j] < colls[i]) {
+                temp = colls[i];
+                colls[i] = colls[j];
+                colls[j] = temp;
+            }
+        }
+    }
+
+    foreach (int obj in colls)
+    {
+        Console.WriteLine(obj);
+    }
+};
+
+//Function call with project array
+bSort(nums);
+
+
