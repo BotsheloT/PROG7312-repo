55a57bc0eeb9116beafc4318d67a21907001c9e4
Botshelo Tlhabanyane <79863670+BotsheloT@users.noreply.github.com>
2022-08-10 10:30:12 +0200

    Virtual Method practice

---
+++ VirtualMethods/Program.cs
@@ -0,0 +1,18 @@
+﻿//Parent Class
+internal class Shape
+{
+    double x, y;
+
+    public Shape()
+    {
+        this.x = 7;
+        this.y = 5;
+    }
+
+    public virtual double calcArea()
+    {
+
+    }
+}
+
+//Main Method
