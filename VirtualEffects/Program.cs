6dc65cac5dd5d91f5c93260a5cca4091caf3719d
Botshelo Tlhabanyane <79863670+BotsheloT@users.noreply.github.com>
2022-08-15 10:26:57 +0200

    Shape application for virtual methods

---
+++ VirtualEffects/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace VirtualEffects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
