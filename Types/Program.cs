13bad994ad82f3b41e2bdcc9bdd7e8e5e987cf68
Botshelo Tlhabanyane <79863670+BotsheloT@users.noreply.github.com>
2022-08-17 10:27:43 +0200

    Print statement

--- Types/Program.cs
+++ Types/Program.cs
@@ -4,9 +4,18 @@ namespace Types
 {
     class Program
     {
+        public static bool isEqual<T>(T x, T y)
+        {
+            if (x.GetType() == typeof(string))
+            {
+                return x.ToString().ToLower().Equals(y.ToString().ToLower());
+            }
+            return x.Equals(y);
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(isEqual("c", "C"));
         }
     }
 }
