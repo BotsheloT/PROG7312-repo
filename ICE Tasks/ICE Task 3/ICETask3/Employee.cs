2caaccab7cfd44960665733501d90999bd317f42
Botshelo Tlhabanyane <79863670+BotsheloT@users.noreply.github.com>
2022-08-29 10:38:27 +0200

    Addition of Employee class

---
+++ ICE Tasks/ICE Task 3/ICETask3/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace ICETask3
+{
+	public class Employee
+	{
+		//Class properties
+		int employeeID;
+		string surname;
+
+		//Class Constructor
+		public Employee(int empID, string empSurname)
+		{
+			employeeID = empID;
+			empSurname = surname;
+		}
+	}
+}
+
