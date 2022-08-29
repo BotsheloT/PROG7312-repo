7568bf1b022f134d00be95c52744a4dbfd06063f
Botshelo Tlhabanyane <79863670+BotsheloT@users.noreply.github.com>
2022-08-29 11:03:01 +0200

    Everyting from step a to c

--- ICE Tasks/ICE Task 3/ICETask3/Program.cs
+++ ICE Tasks/ICE Task 3/ICETask3/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace ICETask3
 {
@@ -6,7 +8,22 @@ namespace ICETask3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Initialised Array with employee surnames
+            string[] surnames = {"Foreman", "Kelso", "Wayne", "Gambino",
+                                 "Tenneson", "West", "Zoldyck", "Freecss",
+                                 "Carter", "Tenma"};
+
+            //List for Employee objects
+            List<Employee> employeeIDs = new List<Employee>();
+
+            //Random object to generate random result
+            Random ran = new Random();
+
+            //Generating and storing list IDs
+            for (int i =0; i < 8; i++)
+            {
+
+            }
         }
     }
 }
