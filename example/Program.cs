9dd9b03390631dfe6f386e736cbcaa9a96a7d160
Botshelo Tlhabanyane <79863670+BotsheloT@users.noreply.github.com>
2022-09-14 10:34:00 +0200

    Program to take first three chars in string

---
+++ example/Program.cs
@@ -0,0 +1,12 @@
+﻿string name = "Charlie";
+
+Console.WriteLine(name[1]);
+
+char first = name[0];
+char second = name[1];
+char third = name[2];
+
+string three = first.ToString() + second.ToString() + third.ToString();
+
+Console.WriteLine(three);
+
