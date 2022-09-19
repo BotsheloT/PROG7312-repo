11937c298992283b810fbd374168950a022a4cb4
Botshelo Tlhabanyane <79863670+BotsheloT@users.noreply.github.com>
2022-09-19 10:12:38 +0200

    Dictionary applcation

---
+++ Dictionary/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Creation of dictionary
+            Dictionary<int, string> employees = new Dictionary<int, string>();
+        }
+    }
+}
+
