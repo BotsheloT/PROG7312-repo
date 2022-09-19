2850ced19d639e006eb9b1030af29b5cb8728876
Botshelo Tlhabanyane <79863670+BotsheloT@users.noreply.github.com>
2022-09-19 10:19:37 +0200

    Additions to Dictionary

--- Dictionary/Program.cs
+++ Dictionary/Program.cs
@@ -10,6 +10,13 @@ namespace Dictionary
         {
             //Creation of dictionary
             Dictionary<int, string> employees = new Dictionary<int, string>();
+
+            //Adding items to Dictionary
+            employees.Add(1, "Chris Hanson");
+            employees.Add(2, "Steve Backster");
+            employees.Add(3, "Fin Murtins");
+            employees.Add(4, "Simon West");
+            employees.Add(5, "Bojack Horseman");            
         }
     }
 }
