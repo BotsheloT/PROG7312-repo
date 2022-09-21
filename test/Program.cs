54a296a2aaa329c0332427da5e6a98d3cf66784c
Botshelo Tlhabanyane <79863670+BotsheloT@users.noreply.github.com>
2022-09-21 19:03:23 +0200

    First Commit

---
+++ test/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Random object to create random numbers
+            Random rnd = new Random();
+
+            //List for Call Numbers
+            List<string> callNumbs = new List<string>();
+
+            //List for author names
+            List<string> authors = new List<string>();
+
+            //Adding authors to author list
+            authors.Add("GOL");
+            authors.Add("CHA");
+            authors.Add("JAN");
+            authors.Add("WIL");
+            authors.Add("HOM");
+            authors.Add("SOC");
+            authors.Add("ARI");
+            authors.Add("MAR");
+            authors.Add("FRI");
+            authors.Add("MAC");
+            authors.Add("SAL");
+            authors.Add("KAF");
+            authors.Add("GAR");
+            authors.Add("WES");
+            authors.Add("MAL");
+            authors.Add("CAR");
+            authors.Add("MIL");
+            authors.Add("TRA");
+            authors.Add("JEN");
+            authors.Add("MIC");
+            authors.Add("GLO");
+            authors.Add("OKA");
+
+            for (int i = 0; i < 10; i++)
+            {
+                int ran = rnd.Next(1, 99);
+
+                string rd = ran.ToString();
+
+                if (rd.Length == 2)
+                {
+                    string temp = rd;
+                    rd = "0" + temp;
+                }
+                else if (rd.Length == 1)
+                {
+                    string temp = rd;
+                    rd = "00" + temp;
+                }
+
+                callNumbs.Add(rd + authors[rnd.Next(1, 22)]);
+            }
+
+            foreach (string item in callNumbs)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
+
