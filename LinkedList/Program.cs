063527a6178ddb9e1c43dc7a1799330e8fbf8fdd
Botshelo Tlhabanyane <79863670+BotsheloT@users.noreply.github.com>
2022-10-26 11:30:00 +0200

    Functional changes

--- LinkedList/Program.cs
+++ LinkedList/Program.cs
@@ -1,39 +1,102 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace LinkedList
 {
-    class Program
+    internal class Program
     {
         static void Main(string[] args)
         {
-            LinkedList<string> cities = new LinkedList<string>();
-            cities.AddLast("Liverpool");
-            cities.AddLast("Shanghai");
-            cities.AddLast("Wein");
-            cities.AddLast("Barcelona");
-            cities.AddLast("Napoli");
+            Tree tree = new Tree();
 
-            //Add Stockholm as the first city
-            cities.AddFirst("Stockholm");
+            Node nRoot = new Node(5, null);
 
-            //Add Malmo after Shanghai
-            LinkedListNode<string> shangNode = cities.Find("Shanghai");
-            cities.AddAfter(shangNode, "Shanghai");
+            tree.Root = nRoot;
 
-            //Add Triest Before Shanghai
-            cities.AddBefore(shangNode,"Shanghai");
+            /*nRoot.Value = 5;
+            nRoot.Parent = null;*/
 
-            //Displaying the Nodes
-            foreach (string item in cities)
+            /*Node child1 = new Node(3, nRoot);
+            Node child2 = new Node(2, nRoot);
+
+            nRoot.Children.Add(child1);
+            nRoot.Children.Add(child2);*/
+
+            nRoot.addChild(3, nRoot);
+            nRoot.addChild(2, nRoot);
+
+
+            foreach (Node node in nRoot.Children)
             {
-                Console.WriteLine(item);
+                Console.WriteLine($"Parent = {node.Parent.Value}, value = {node.Value}");
+            }
+            Console.WriteLine("==================");
 
-            //Display what comes before Shanghai
-            Console.WriteLine("==============================================");
-            Console.WriteLine(shangNode.Previous.Value);
-            Console.WriteLine("==============================================");            
-            
+
+            Node child = nRoot.insert(10);
+            Console.WriteLine($"Parent = {child.Parent.Value}, Value = {child.Value}");
+
+
+            Console.ReadKey();
+        }
+
+
+    }
+
+    public class Tree
+    {
+        public Node Root { get; set; }
+        public List<Node> Nodes { get; set; }
+        public LinkedList<Node> LinkedList = new LinkedList<Node>();
+
+        public Tree StartBranch(int parentValue)
+        {
+            if (LinkedList.Count == 0)
+            {
+                Node rootNode = new Node(parentValue, null);
+                Nodes.Add(rootNode);
+                LinkedList.AddFirst(rootNode);
+            }
+            else
+            {
+                Node newParent = LinkedList.First().insert(parentValue);
+                Nodes.Add(newParent);
+                LinkedList.AddLast(newParent);
+            }
+        }
+    }
+
+    public class Node
+    {
+        public int Value { get; set; }
+        public Node Parent { get; set; }
+        public List<Node> Children { get; set; }
+
+        public Node()
+        {
+            //this.Children = new List<Node>();
+        }
+
+        public Node(int value, Node parent)
+        {
+            Value = value;
+            Parent = parent;
+            Children = new List<Node>();
+        }
+
+        public void addChild(int value, Node parent)
+        {
+            Node newNode = new Node(value, parent);
+            parent.Children.Add(newNode);
+        }
+
+        public Node insert(int value)
+        {
+            Node newNode = new Node(value, Parent = this);
+            this.Children.Add(newNode);
+
+            return newNode;
         }
     }
 }
